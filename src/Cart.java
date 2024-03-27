1cdf4e1651d5612a2a3a0356c034b3ae0e5226a9
kimduyminh <minhkimfp212@gmail.com>
2024-03-27 11:27:12 +0700

    First push

---
+++ src/Cart.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class Cart {
+    public static final int MAX_NUMBER_ORDERED=20;
+    private int quantity;
+    private DigitalVideoDisc[] query=new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+
+    public Cart(){
+        this.quantity=0;
+    }
+    public void add(DigitalVideoDisc dvd){
+
+    }
+}

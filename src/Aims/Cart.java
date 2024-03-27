9c048bd0d235fe03a6e1a334adab6c93887d060b
kimduyminh <minhkimfp212@gmail.com>
2024-03-27 23:52:44 +0700

    Add package Aims

---
+++ src/Aims/Cart.java
@@ -0,0 +1,14 @@
+package Aims;
+
+public class Cart {
+    public static final int MAX_NUMBER_ORDERED=20;
+    private int quantity;
+    private DigitalVideoDisc[] query=new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+
+    public Cart(){
+        this.quantity=0;
+    }
+    public void add(DigitalVideoDisc dvd){
+
+    }
+}
